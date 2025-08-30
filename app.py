48aba93088ea75eb52dc673ef80bfc35b0eb92aa
TanishaKothari <kotharitanisha50@gmail.comgit config --global user.email kotharitanisha50@gmail.com>
2025-08-30 16:50:43 +1000

    Add functions to store data, match jobs and needs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Connect to mysql database
 mydb = mysql.connector.connect(
   host=os.getenv("DB_HOST"),
   user=os.getenv("DB_USER"),
@@ -11,5 +12,20 @@ mydb = mysql.connector.connect(
 )
 cursor = mydb.cursor()
 cursor.execute("USE gigapp")
+
+# Create tables in db
 cursor.execute("CREATE TABLE IF NOT EXISTS jobs (user_id VARCHAR(36) NOT NULL, job_name VARCHAR(100) NOT NULL, PRIMARY KEY (user_id, job_name))")
 cursor.execute("CREATE TABLE IF NOT EXISTS needs (user_id VARCHAR(36) NOT NULL, need_name VARCHAR(100) NOT NULL, PRIMARY KEY (user_id, need_name))")
+
+# Function: insert into jobs
+def insertUserJob(user_id, job_name):
+    cursor.execute("INSERT INTO jobs VALUES (%s, %s)", (user_id, job_name))
+
+# Function: insert into needs
+def insertUserNeed(user_id, need_name):
+    cursor.execute("INSERT INTO needs VALUES (%s, %s)", (user_id, need_name))
+    match(need_name)
+
+# Match person with a need with people who can do that job
+def match(need_name):
+    cursor.execute("SELECT user_id FROM jobs WHERE job_name = %s", need_name)
