a37797eae8afbae7000edf781691ddf5a8834ecf
mzee0979 <daniyal.avantgarde@gmail.com>
2025-08-31 10:24:31 +1000

    removed unnecessary files

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <div className="h-screen grid grid-rows-[auto_1fr] grid-cols-3">
-      <div className="col-span-3 bg-[var(--color-primary)] h-[75px] flex relative items-center">
+      <div id="header" className="col-span-3 bg-[var(--color-primary)] h-[75px] flex relative items-center">
 
         <Image
         src="/gigimon.png"
@@ -30,13 +30,13 @@ export default function Home() {
         <h1 className="text-white text-3xl" style={{ fontFamily: "var(--font-retro)" }}>help your community</h1>
       </div>
 
-      <div className="col-span-2">
+      <div id="map" className="col-span-2">
         <MapClientWrapper/>
       </div>
         
-      <div className="col-span-1 grid grid-rows-[2fr_1fr] h-full">
+      <div id="list-and-buttons" className="col-span-1 grid grid-rows-[2fr_1fr] h-full">
         
-        <div id="list" className="bg-[var(--color-quaternary)] flex flex-col gap-2 p-2 overflow-y-auto min-h-0 h-full" style={{ minHeight: 0 }}>
+        <div id="list" className="bg-[var(--color-quaternary)] flex flex-col gap-2 p-2 overflow-y-auto min-h-0" style={{ minHeight: 0 }}>
           {needs.map((need, idx) => (
             <NeedCard
               key={idx}
@@ -59,6 +59,8 @@ export default function Home() {
           email="sammy@gmail.com"> 
           </NeedCard>
           
+          
+        
         </div>
 
         <div id="buttons" className="grid grid-rows-2 bg-[var(--color-primary)] place-items-center">
